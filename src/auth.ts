c832c2aad2fc06c7b3db9b8c6d4322a778ee55af
harshit Mishra <miananya2@gmail.com>
2024-09-04 02:12:12 +0530

    token, session

--- src/auth.ts
+++ src/auth.ts
@@ -1,11 +1,41 @@
 import NextAuth from "next-auth";
-import { db } from "./lib/db";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import authConfig from "./auth.config";
+import { getUserById } from "./data/user";
+import { getAccountByUserId } from "./data/accounts";
+import { db } from "./lib/db";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   pages: {
     signIn: "/auth/login",
+  },
+  callbacks: {
+    async session({ session, token }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (session.user) {
+        session.user.name = token.name;
+        session.user.email = token.email as string;
+      }
+
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      const existingAccount = await getAccountByUserId(existingUser.id);
+
+      token.email = existingUser.email;
+      token.name = existingUser.name;
+
+      return token;
+    },
   },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
